574d730b97d45485fab7a4ccef700294ba0fadfa
Zahaib Bin Rizwan <fa22-bcs-132@cuivehari.edu.pk>
2026-06-23 17:15:14 +0500

    Test auto deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, CI/CD Pipeline!"
+    return "Hello from Auto Deployment!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello, CI/CD Pipeline!" in response.data
+    assert b"Hello from Auto Deployment!" in response.data
