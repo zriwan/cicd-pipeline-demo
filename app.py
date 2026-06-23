f9aea91ba9c443cb7b84e0ca75ca53a2dd7d1a27
Zahaib Bin Rizwan <fa22-bcs-132@cuivehari.edu.pk>
2026-06-23 12:03:07 +0500

    Fix app output

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "wrong output"
+    return "Hello, CI/CD Pipeline!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
