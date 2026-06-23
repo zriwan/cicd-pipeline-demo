f0147df7e44f5c4d7b1270ece38aa93eca4c7423
Zahaib Bin Rizwan <fa22-bcs-132@cuivehari.edu.pk>
2026-06-23 23:10:45 +0500

    Add HTML and CSS landing page

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello from Auto Deployment!" in response.data
+    assert b"CI/CD Pipeline Successfully Deployed" in response.data
